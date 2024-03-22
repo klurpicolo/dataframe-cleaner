67d78172b1e7de017825184a049668a731cc538d
waritboonmasiri <warit.boonmasiri@gmail.com>
2024-03-22 12:12:30 +1100

    Implement the proper file format

--- backend/common/minio_client.py
+++ backend/common/minio_client.py
@@ -4,6 +4,7 @@ import pandas as pd
 from minio import Minio
 
 
+# This isn't not secure, just for sake of setup and demo.
 client = Minio("localhost:9000",
         secure=False,
         cert_check=False
@@ -11,10 +12,13 @@ client = Minio("localhost:9000",
 
 dataframe_bucket_name = 'dataframes'
 
-# TODO use the proper encrypt data type
+
 def upload_dataframe(dataframe_id: str, version_id: str, df: pd.DataFrame):
-    raw_byte = BytesIO(df.to_csv(index=False).encode('utf-8'))
-    destination_file = f"{dataframe_id}_{version_id}"
+    buffer = BytesIO()
+    df.to_parquet(buffer, engine='pyarrow', compression='brotli')
+    buffer.seek(0)
+
+    destination_file = f"{dataframe_id}_{version_id}.br"
     found = client.bucket_exists(dataframe_bucket_name)
     if not found:
         client.make_bucket(dataframe_bucket_name)
@@ -25,7 +29,7 @@ def upload_dataframe(dataframe_id: str, version_id: str, df: pd.DataFrame):
     client.put_object(
         dataframe_bucket_name,
         destination_file,
-        raw_byte,
+        buffer,
         -1,  # Specify data size
         part_size=10*1024*1024
     )
@@ -35,13 +39,11 @@ def upload_dataframe(dataframe_id: str, version_id: str, df: pd.DataFrame):
     )
 
 def get_dataframe(dataframe_id: str, version_id: str) -> pd.DataFrame:
-    raw_byte = client.get_object(dataframe_bucket_name, f"{dataframe_id}_{version_id}")
-
+    raw_byte = client.get_object(dataframe_bucket_name, f"{dataframe_id}_{version_id}.br")
     buffer = BytesIO()
     for d in raw_byte.stream(32*1024):
         buffer.write(d)
     buffer.seek(0)
 
-    df = pd.read_csv(buffer)
-
+    df = pd.read_parquet(buffer, engine='pyarrow')
     return df

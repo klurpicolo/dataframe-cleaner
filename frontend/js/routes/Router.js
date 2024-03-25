60e1f8ce58d5394e15555189b2b0c315209a7200
waritboonmasiri <warit.boonmasiri@gmail.com>
2024-03-25 23:17:32 +1100

    Remove unused files

--- frontend/js/routes/Router.js
+++ frontend/js/routes/Router.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-import About from "../pages/About";
 import DataframeCleanerPage from "../pages/DataframeCleanerPage";
-import DataframeV1 from "../pages/DataframeV1";
-import DataframeV2 from "../pages/DataframeV2";
-import Home from "../pages/Home";
 
 import { ROUTES } from "./RouterConfig";
 
@@ -17,26 +13,6 @@ const Router = () => {
   return (
     <div>
       <Routes>
-        <Route
-          element={<RouteWithRole Element={Home} />}
-          exact
-          path={ROUTES.Home}
-        />
-        <Route
-          element={<RouteWithRole Element={About} />}
-          exact
-          path={ROUTES.About}
-        />
-        <Route
-          element={<RouteWithRole Element={DataframeV1} />}
-          exact
-          path={ROUTES.DataframeV1}
-        />
-        <Route
-          element={<RouteWithRole Element={DataframeV2} />}
-          exact
-          path={ROUTES.DataframeV2}
-        />
         <Route
           element={<RouteWithRole Element={DataframeCleanerPage} />}
           exact

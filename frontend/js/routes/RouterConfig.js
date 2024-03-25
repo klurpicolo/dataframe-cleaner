9d9483a471b51a75a96bd702a5c160cb9aaadc52
waritboonmasiri <warit.boonmasiri@gmail.com>
2024-03-25 23:06:04 +1100

    Update data cleaner to be root path

--- frontend/js/routes/RouterConfig.js
+++ frontend/js/routes/RouterConfig.js
@@ -1,7 +1,3 @@
 export const ROUTES = {
-  Home: "/",
-  About: "/about",
-  DataframeV1: "dataframev1",
-  DataframeV2: "dataframeV2",
-  DataframeCleanerPage: "dataframe-cleaner",
+  DataframeCleanerPage: "/",
 };

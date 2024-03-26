a86f8250b17fe000d7d8506d55b23e081ccdbb5b
waritboonmasiri <warit.boonmasiri@gmail.com>
2024-03-26 23:23:50 +1100

    Add more test on data processor

--- backend/common/tests/test_data_processors.py
+++ backend/common/tests/test_data_processors.py
@@ -7,6 +7,42 @@ from backend.common.data_processors import infer_col, infer_df
 
 class TestDataTypes(unittest.TestCase):
 
+    def test_defer_boolean(self):
+        data = ['true', 'false', 'true', 'false', 'false']
+        ser = pd.Series(data)
+
+        result = infer_col(ser)
+        self.assertEqual(result.dtype, 'bool')
+
+
+    def test_defer_boolean_non_clean(self):
+        data = ['true', 'false', 'true', 'false', 'false', 'false', 'false', 'true', 'false', 'N/A']
+        ser = pd.Series(data)
+
+        result = infer_col(ser)
+        self.assertEqual(result.dtype, 'bool')
+
+    def test_defer_category(self):
+        # all 60 values, with 4 unique value. So the theshold is 4/60 = 0.06
+        data = ['A', 'B', 'D', 'D', 'C', 'A', 'B', 'D', 'C', 'A', 'B', 'D', 'D', 'C', 'A', 'A','D', 'D', 'A', 'A',
+            'A', 'B', 'D', 'D', 'C', 'A', 'B', 'D', 'C', 'A', 'B', 'D', 'D', 'C', 'A', 'A','D', 'D', 'A', 'A',
+            'A', 'B', 'D', 'D', 'C', 'A', 'B', 'D', 'C', 'A', 'B', 'D', 'D', 'C', 'A', 'A','D', 'D', 'A', 'A']
+        ser = pd.Series(data)
+
+        result = infer_col(ser)
+        self.assertEqual(result.dtype, 'category')
+
+    # def test_defer_string(self):
+    #     # all 60 values, with 4 unique value. So the theshold is 4/60 = 0.06
+    #     data = ['A', 'B', 'D', 'D', 'C', 'A', 'B', 'D', 'C', 'A', 'B', 'D', 'D', 'C', 'A', 'A','D', 'D', 'A', 'A',
+    #         'A', 'B', 'D', 'D', 'C', 'A', 'B', 'D', 'C', 'A', 'B', 'D', 'D', 'C', 'A', 'A','D', 'D', 'A', 'A',
+    #         'A', 'B', 'D', 'D', 'C', 'A', 'B', 'D', 'C', 'A', 'B', 'D', 'D', 'C', 'A', 'A','D', 'D', 'A', 'A']
+    #     ser = pd.Series(data)
+
+    #     result = infer_col(ser)
+    #     print(f'test_defer_category {result.dtypes}')
+    #     self.assertEqual(result.dtype, 'category')
+
     def test_data_type_inference(self):
         # Create a sample DataFrame for testing
         data = {
@@ -23,7 +59,7 @@ class TestDataTypes(unittest.TestCase):
         print(f'klir {result_df.dtypes}')
         self.assertEqual(result_df['A'].dtype, 'datetime64[ns]')
         self.assertEqual(result_df['B'].dtype, 'int64')
-        self.assertEqual(result_df['C'].dtype, 'category')
+        self.assertEqual(result_df['C'].dtype, 'object')
         self.assertEqual(result_df['D'].dtype, 'bool')
         self.assertEqual(result_df['E'].dtype, 'object')
 
@@ -42,19 +78,10 @@ class TestDataTypes(unittest.TestCase):
         print(f'klir {result_df.dtypes}')
         self.assertEqual(result_df['A'].dtype, 'datetime64[ns]')
         self.assertEqual(result_df['B'].dtype, 'float64') # ToDO check this
-        self.assertEqual(result_df['C'].dtype, 'category')
+        self.assertEqual(result_df['C'].dtype, 'object')
         # self.assertEqual(result_df['D'].dtype, 'boolean')
         self.assertEqual(result_df['E'].dtype, 'object')
 
-    def test_defer_boolean_with_defect_case(self):
-        data = {
-            'Boolean with defect': [True, False, True, False]
-        }
-        ser = pd.Series(data)
-
-        result = infer_col(ser)
-        print(f'klur {result}')
-        self.assertEqual(result, 'asd')
 
 if __name__ == '__main__':
     unittest.main()
